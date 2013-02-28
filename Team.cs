c3fb19a20be72d6bd82037e58a9a67bc4b94d768
Jeff Blankenburg <github@jeffblankenburg.com>
2013-02-28 16:27:08 -0500

    Initial Commit

---
+++ Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldSeriesHistory
+{
+    public class Team
+    {
+        public string City { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Abbreviation { get; set; }
+
+    }
+}
---
+++ WorldSeries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldSeriesHistory
+{
+    public class WorldSeries
+    {
+        public Team NationalLeagueTeam { get; set; }
+        public Team AmericanLeagueTeam { get; set; }
+    }
+}
